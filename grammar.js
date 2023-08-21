7b6117d0bebb7142c7857e18e1e63462ebd28ba2
Jay <Jordan.Cuevas@precisecal.com>
2023-08-21 00:51:44 -0400

    Fix grammar

--- grammar.js
+++ grammar.js
@@ -1,5 +1,9 @@
 module.exports = grammar({
   name: 'whitespace',
+  extras: $ => [
+    // $.comment,
+    /[^ \t\n]/,
+  ],
   rules: {
     source_file: $ => repeat($._op),
     _op: $ => choice(
@@ -10,48 +14,58 @@ module.exports = grammar({
       $.op_stack
     ),
 
-    S: $ => 'S',
-    L: $ => 'L',
-    T: $ => 'T',
+    _S: $ => ' ',
+    _L: $ => '\n',
+    _T: $ => '\t',
 
-    imp_io: $ => seq($.T, $.L),
-    imp_stack: $ => $.S,
-    imp_arithmetic: $ => seq($.T, $.S),
-    imp_flow: $ => $.L,
-    imp_heap: $ => seq($.T, $.T),
+    S: $ => ' ',
+    L: $ => '\n',
+    T: $ => '\t',
 
-    read_char: $ => seq(field('imp', $.imp_io), $.T, $.S),
-    read_num: $ => seq(field('imp', $.imp_io), $.T, $.T),
-    print_char: $ => seq(field('imp', $.imp_io), $.S, $.S),
-    print_num: $ => seq(field('imp', $.imp_io), $.S, $.T),
+    // S: $ => 'S',
+    // L: $ => 'L',
+    // T: $ => 'T',
+
+    comment: $ => /\w/,
+
+    _imp_io: $ => seq($._T, $._L),
+    _imp_stack: $ => $._S,
+    _imp_arithmetic: $ => seq($._T, $._S),
+    _imp_flow: $ => $._L,
+    _imp_heap: $ => seq($._T, $._T),
+
+    read_char: $ => seq($._imp_io, $._T, $._S),
+    read_num: $ => seq($._imp_io, $._T, $._T),
+    print_char: $ => seq($._imp_io, $._S, $._S),
+    print_num: $ => seq($._imp_io, $._S, $._T),
     op_io: $ => choice($.read_char, $.read_num, $.print_char, $.print_num),
 
-    add: $ => seq(field('imp', $.imp_arithmetic), $.S, $.S),
-    sub: $ => seq(field('imp', $.imp_arithmetic), $.S, $.T),
-    mul: $ => seq(field('imp', $.imp_arithmetic), $.S, $.L),
-    div: $ => seq(field('imp', $.imp_arithmetic), $.T, $.S),
-    mod: $ => seq(field('imp', $.imp_arithmetic), $.T, $.T),
+    add: $ => seq($._imp_arithmetic, $._S, $._S),
+    sub: $ => seq($._imp_arithmetic, $._S, $._T),
+    mul: $ => seq($._imp_arithmetic, $._S, $._L),
+    div: $ => seq($._imp_arithmetic, $._T, $._S),
+    mod: $ => seq($._imp_arithmetic, $._T, $._T),
     op_arithmetic: $ => choice($.add, $.sub, $.mul, $.div, $.mod),
 
-    push: $ => seq(field('imp', $.imp_stack), $.S, $.num),
-    dup: $ => seq(field('imp', $.imp_stack), $.L, $.S),
-    swap: $ => seq(field('imp', $.imp_stack), $.L, $.T),
-    discard: $ => seq(field('imp', $.imp_stack), $.L, $.L),
-    copy: $ => seq(field('imp', $.imp_stack), $.T, $.S, $.num),
-    slide: $ => seq(field('imp', $.imp_stack), $.T, $.L, $.num),
+    push: $ => seq($._imp_stack, $._S, field('num', $.num)),
+    dup: $ => seq($._imp_stack, $._L, $._S),
+    swap: $ => seq($._imp_stack, $._L, $._T),
+    discard: $ => seq($._imp_stack, $._L, $._L),
+    copy: $ => seq($._imp_stack, $._T, $._S, field('num', $.num)),
+    slide: $ => seq($._imp_stack, $._T, $._L, field('num', $.num)),
     op_stack: $ => choice($.push, $.dup, $.swap, $.discard, $.copy, $.slide),
 
-    store: $ => seq(field('imp', $.imp_heap), $.S),
-    load: $ => seq(field('imp', $.imp_heap), $.L),
+    store: $ => seq($._imp_heap, $._S),
+    load: $ => seq($._imp_heap, $._T),
     op_heap: $ => choice($.store, $.load),
 
-    label: $ => seq(field('imp', $.imp_flow), $.S, $.S, $.label_name),
-    call: $ => seq(field('imp', $.imp_flow), $.S, $.T, $.label_name),
-    jump: $ => seq(field('imp', $.imp_flow), $.S, $.L, $.label_name),
-    jump_zero: $ => seq(field('imp', $.imp_flow), $.T, $.S, $.label_name),
-    jump_neg: $ => seq(field('imp', $.imp_flow), $.T, $.T, $.label_name),
-    return: $ => seq(field('imp', $.imp_flow), $.T, $.L),
-    exit: $ => seq(field('imp', $.imp_flow), $.L, $.L),
+    label: $ => seq($._imp_flow, $._S, $._S, field('label_name', $.label_name)),
+    call: $ => seq($._imp_flow, $._S, $._T, field('label_name', $.label_name)),
+    jump: $ => seq($._imp_flow, $._S, $._L, field('label_name', $.label_name)),
+    jump_zero: $ => seq($._imp_flow, $._T, $._S, field('label_name', $.label_name)),
+    jump_neg: $ => seq($._imp_flow, $._T, $._T, field('label_name', $.label_name)),
+    return: $ => seq($._imp_flow, $._T, $._L),
+    exit: $ => seq($._imp_flow, $._L, $._L),
     op_flow: $ => choice(
       $.label,
       $.call,
@@ -68,12 +82,12 @@ module.exports = grammar({
       $.L
     ),
     num: $ => seq(
-      field('sign', choice($.S, $.T)),
+      field('sign', choice($._S, $._T)),
       field('value', repeat(choice($.S, $.T))),
-      // repeat(choice($.S, $.T)),
+      // repeat(choice($._S, $._T)),
       // field('value', repeat(choice('S', 'T'))),
       // token(repeat(choice('S', 'T'))),
-      field('terminator', $.L),
+      field('terminator', $._L),
     )
   }
 });
